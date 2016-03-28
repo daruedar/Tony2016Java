363d7d08754983c4a2da6ec673fa326e59e2e3ff
Diego Armando Rueda Robles <daruedar@hotmail.com>
2016-03-28 17:52:45 -0500

    Update BaseDeDatos.java
    El objetivo de esta clase es realizar la conexion con la BD, usando el driver respectivo, la interfaz publica es simplemente un metodo que nos devuelve la conexion a la BD,Oserve que el constructor es el por Default

--- BaseDeDatos.java
+++ BaseDeDatos.java
@@ -2,50 +2,25 @@ package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 import javax.swing.JOptionPane;
-		/**
-		* Conecta a una base de datos en la ruta pasada como argumento.
-		* Si el archivo señalado en la ruta no existe, se crea.
-		* @param ruta - La ruta de la DB a conectar.
-		* @return La conexión a la ruta.
-	*/
 public final class BaseDeDatos 
-	{/**Este metodo se encarga de cargar el driver, obtener la conexion y retornar  el recurso conexion*/
+	{
 		public static Connection conectarA(String ruta)
 			{
-				try {Class.forName("org.sqlite.JDBC");}//Aquí cargamos el driver de SQLITE.
+				try {Class.forName("org.sqlite.JDBC");}//AquÃ­ cargamos el driver de SQLITE.
 				catch (ClassNotFoundException e) 
 					{
 						e.printStackTrace();//Esto se ejecuta si hay un error con el driver de la base de datos.
 						JOptionPane.showMessageDialog(null, "Se produjo un error con el driver, revisar La  BaseDeDatos.class");
 					}
-				Connection conn = null;//Declaramos la conexión:
-				try {conn = DriverManager.getConnection("jdbc:sqlite:" + ruta);}//Aquí se obtiene la conexión:
+				Connection conn = null;//Declaramos la conexiÃ³n:
+				try {conn = DriverManager.getConnection("jdbc:sqlite:" + ruta);}//AquÃ­ se obtiene la conexiÃ³n:
 				catch (SQLException e) 
 					{
 						e.printStackTrace();//Esto se ejecuta si hay un error en la base de datos:
 						JOptionPane.showMessageDialog(null, "Se produjo un error al intentar conectar a la Base de datos, revise la clase BaseDeDatosen el package model,");
 					}
-				return conn;//Devolvemos la conexión:
+				return conn;//Devolvemos la conexiÃ³n:
 			}//Fin del metodo conectarA
-		public static void main(String[] args) 
-			{
-				//Pasamos una ruta a la cual conectar:
-				Connection conn = BaseDeDatos.conectarA("Tony.db");
-				/*
-				* Aquí se supone que hacemos lo que necesitamos en la base de datos
-				* ya sea una consulta, creación de una tabla, actualización de la
-				* misma, etc.
-				*/
-				try {
-						//Y para terminar cerramos la conexión
-						conn.close();
-					}
-				catch (SQLException e) {
-						//Esto se ejecuta si hay algún problema al realizar la conexión.
-						e.printStackTrace();
-					}
-			}
 	}
 
