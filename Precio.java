623345b09275a3efeeea8583c7a085088d9ed37f
Diego Armando Rueda Robles <daruedar@hotmail.com>
2016-03-28 18:18:17 -0500

    Clase auxiliar para formar un ArrayList<Precio>
    Brinda metodos de soporte para la creacion de un arrayList con el contenido de la tabla Precios

--- Precio.java
+++ Precio.java
@@ -5,23 +5,8 @@ public class Precio
 		private int id;
 		private String nombre;
 		private double precio;
-		
-		public Precio(int id,String name, double price)
-			{
-				this.nombre=name;
-				this.precio=price;
-				this.id=id;
-			}
-
-		public int getId() {
-			return id;
-		}
-
-		public String getNombre() {
-			return nombre;
-		}
-
-		public double getPrecio() {
-			return precio;
-		}
+		public Precio(int id,String name, double price){this.nombre=name;this.precio=price;this.id=id;}//Contructor
+		public int getId() {return id;}
+		public String getNombre() {return nombre;}
+		public double getPrecio() {return precio;}
 	}
