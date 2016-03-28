d75a67dec27979702d6e6ad593e9646c5e14b652
Diego Armando Rueda Robles <daruedar@hotmail.com>
2016-03-28 18:13:49 -0500

    Controla el model and view package
    Quien se encrga de gestionar toda la aplicacion,tanto el modelo como la vista,tiene un constructor privado, y un metodo getInstance() para obtener una UNICA instancia, en otras palabras  JUTSU SINGLETOON

--- Logica.java
+++ Logica.java
@@ -15,29 +15,20 @@ public class Logica
 		private static Logica instance;
 		private Querys consultas;
 		private Bunker reportes;
-		
  		public static Logica getInstance() {
 			if(instance==null){instance= new Logica();}
 			return instance;
 		}
 		public ArrayList<Precio> getPrecios() {precios=null;this.precios=consultas.getPrecios();return precios;}
-
-		public Logica() {
+		private Logica() {
 			precios= new ArrayList<Precio>();
 			consultas= new Querys();
 			reportes= new Bunker();
 		}
-		
-	
 		public static void main(String[] args) throws DocumentException, IOException 
 		{
 			Logica app= new Logica();
 			app.getPrecios();
 			app.reportes.reportPrice(precios);
-			
-			
-			
-
 		}
-
 	}
